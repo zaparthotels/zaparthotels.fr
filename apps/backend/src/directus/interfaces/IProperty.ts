839bcc84ad4b8e1b2e759074fd0ac554570cb340
leo-planus-ext_sonepar <leo.planus_ext@sonepar.com>
2025-05-18 11:56:12 +0200

    types

--- apps/backend/src/directus/interfaces/IProperty.ts
+++ apps/backend/src/directus/interfaces/IProperty.ts
@@ -1,13 +1,15 @@
 export interface IProperty {
   id: string;
   beds24id: string;
-  lockId: string;
   arrivalNotificationTime: string;
   departureNotificationTime: string;
-  fallbackLockCode?: string;
   translations: {
     subject: string;
     notification_arrival: string;
     notification_departure: string;
+  }[];
+  locks: {
+    lockId: string;
+    fallbackLockCode: string;
   }[];
 }
